1092f355a0c3a9fbccc8e92b103f6902220056a8
Steven Cheng <stevenc81@gmail.com>
2013-05-27 01:54:34 +0800

    - first working prototype

--- main.py
+++ main.py
@@ -30,6 +30,12 @@ from google.appengine.api import memcache
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
+import re
+from datetime import datetime
+from datetime import timedelta
+from BeautifulSoup import BeautifulSoup
+from calendar import monthrange
+
 
 # CLIENT_SECRETS, name of a file containing the OAuth 2.0 information for this
 # application, including client_id and client_secret, which are found
@@ -61,6 +67,11 @@ decorator = oauth2decorator_from_clientsecrets(
     scope='https://www.googleapis.com/auth/calendar',
     message=MISSING_CLIENT_SECRETS_MESSAGE)
 
+
+def _nbsp_trim(string):
+    return string.replace('&nbsp;', '')
+
+
 class MainHandler(webapp2.RequestHandler):
 
     @decorator.oauth_aware
@@ -90,7 +101,10 @@ class ImportHandler(webapp2.RequestHandler):
                 <html>
                   <body>
                     <form method="post">
-                        <p>Crew ID: <input type="text name="crew_id" /></p>
+                        <p>Crew ID: <input type="text" name="crew_id" /></p>
+                        <p>Password: <input type="password" name="password" /></p>
+                        <p>Import year: 2013</p>
+                        <p>Month: <input type="month" name="month" /></p>
                         <p><input type="submit" value="Submit" /></p>
                     </form>
                   </body>
@@ -102,39 +116,71 @@ class ImportHandler(webapp2.RequestHandler):
     @decorator.oauth_required
     def post(self):
         crew_id = self.request.get('crew_id')
+        password = self.request.get('password')
+        month = self.request.get('month').split('-')[1]
+        strDay, endDay = monthrange(2013, int(month))
 
         r = requests.Session()
         results = r.post(
             "http://cia.china-airlines.com/LoginHandler",
-            params={'userid': '635426',
-                    'password': 'ju635426'}
+            params={'userid': crew_id,
+                    'password': password}
         )
 
         results = r.post(
             "http://cia.china-airlines.com/cia_inq_view_rostreport.jsp",
-            params={'staffNum': '635426',
+            params={'staffNum': crew_id,
                     'strDay': '01',
-                    'strMonth': '05',
+                    'strMonth': month,
                     'strYear': '2013',
-                    'endDay': '31',
-                    'endMonth': '05',
+                    'endDay': endDay,
+                    'endMonth': month,
                     'endYear': '2013',
                     'display_timezone': 'Port Local'}
         )
 
-        # event = {
-        #     'summary': 'Appointment',
-        #     'location': 'Somewhere',
-        #     'start': {
-        #         'dateTime': '2013-06-25T10:00:00.000-07:00'
-        #     },
-        #     'end': {
-        #         'dateTime': '2013-06-25T10:25:00.000-07:00'
-        #     }
-        # }
-        # http = decorator.http()
-        # request = service.events().insert(calendarId='primary', body=event).execute(http=http)
-        self.response.out.write(results.text)
+        soup = BeautifulSoup(results.text)
+
+        created_events = []
+        fly_date = ''
+        for row in soup('tr'):
+            cols = row('td')
+            if len(cols) != 12:
+                continue
+
+            if re.search(r'^\d+\w+\d+&nbsp;$', cols[0].text) is not None:
+                fly_date = _nbsp_trim(cols[0].text)
+
+            if cols[2].text.find('FLY') != -1 and cols[6].text != '&nbsp;':
+                fly_time = _nbsp_trim(cols[6].text)
+                fly_summary = _nbsp_trim(cols[4].text)
+
+                dt = datetime.strptime(fly_date + fly_time, '%d%b%y%H%M')
+                edt = dt + timedelta(hours=8)
+
+                event = {
+                    'summary': str(fly_summary),
+                    'start': {
+                        'dateTime': dt.strftime('%Y-%m-%d')
+                        + 'T'
+                        + dt.strftime('%H:%M:%S')
+                        + '.000+08:00'
+                    },
+                    'end': {
+                        'dateTime': edt.strftime('%Y-%m-%d')
+                        + 'T'
+                        + edt.strftime('%H:%M:%S')
+                        + '.000+08:00'
+                    },
+                    'attendees': [
+                    ]
+                }
+
+                http = decorator.http()
+                created_event = service.events().insert(calendarId='primary', body=event).execute(http=http)
+                created_events.append(created_event)
+
+        self.response.out.write(str(len(created_events)) + ' events imported to Google Calendar')
 
 app = webapp2.WSGIApplication(
     [
--- scraper.py
+++ scraper.py
@@ -1,44 +1,80 @@
 import requests
+import re
+from datetime import datetime
+from BeautifulSoup import BeautifulSoup
+from datetime import timedelta
+from calendar import monthrange
+
+
+def _nbsp_trim(string):
+    return string.replace('&nbsp;', '')
 
 
 def scrape():
+    month = '2013-06'
+    month = month.split('-')[1]
+    strDay, endDay = monthrange(2013, int(month))
+
     r = requests.Session()
     results = r.post(
         "http://cia.china-airlines.com/LoginHandler",
         params={'userid': '635426',
-                'password': 'ju635426'}
+                'password': '$1688$'}
     )
-    # print results.text
-
-    # results = r.post(
-    #     "http://cia.china-airlines.com/cia_rpt_roster_summary.jsp",
-    #     params={'userid': '635426',
-    #             'password': 'ju635426'}
-    # )
-    # print results.text
-
-    # results = r.post(
-    #     "http://cia.china-airlines.com/cia_inq_view_rostsummary.jsp",
-    #     params={'staffNum': '635426',
-    #             'rosterPeriod': '01May2013 - 31May2013',
-    #             'display_timezone': 'Port Local'
-    #     }
-    # )
-    # print results.text
 
     results = r.post(
         "http://cia.china-airlines.com/cia_inq_view_rostreport.jsp",
         params={'staffNum': '635426',
                 'strDay': '01',
-                'strMonth': '05',
+                'strMonth': month,
                 'strYear': '2013',
-                'endDay': '31',
-                'endMonth': '05',
+                'endDay': endDay,
+                'endMonth': month,
                 'endYear': '2013',
-                'display_timezone': 'Port Local'
-        }
+                'display_timezone': 'Port Local'}
     )
-    print results.text
+    # print results.text
+
+    # f = open('req_result.html', 'w')
+    # f.write(results.text)
+    # f.close()
+
+    # f = open('sample.html')
+    soup = BeautifulSoup(results.text)
+    # f.close()
+
+    fly_date = ''
+    for row in soup('tr'):
+        cols = row('td')
+        if len(cols) != 12:
+            continue
+        if re.search(r'^\d+\w+\d+&nbsp;$', cols[0].text) is not None:
+            fly_date = _nbsp_trim(cols[0].text)
+        if cols[2].text.find('FLY') != -1 and cols[6].text != '&nbsp;':
+            fly_time = _nbsp_trim(cols[6].text)
+            fly_summary = _nbsp_trim(cols[4].text)
+
+            dt = datetime.strptime(fly_date + fly_time, '%d%b%y%H%M')
+            edt = dt + timedelta(hours=8)
+
+            event = {
+                'summary': str(fly_summary),
+                'start': {
+                    'dateTime': dt.strftime('%Y-%m-%d')
+                    + 'T'
+                    + dt.strftime('%H:%M:%S')
+                    + '.000+08:00'
+                },
+                'end': {
+                    'dateTime': edt.strftime('%Y-%m-%d')
+                    + 'T'
+                    + edt.strftime('%H:%M:%S')
+                    + '.000+08:00'
+                },
+                'attendees': [
+                ]
+            }
+            print event
 
 if __name__ == "__main__":
     scrape()
